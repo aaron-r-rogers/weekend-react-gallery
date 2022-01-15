23ad4836d80e48e484b05226e8bce06cd913edc3
aaron-r-rogers <rogers.aaron.r@gmail.com>
2022-01-15 10:15:39 -0600

    added component folders/files and images for testing

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, title: 'Chair', description: 'Photo of a chair I restored for a friend last year.', path: 'images/chair.jpg', likes: 0 },
+    { id: 2, title: 'Frances', description: 'Picture of my daughter in her element.', path: 'images/frances.jpg', likes: 0 },
+    { id: 3, title: 'Gondola', description: 'Frances on the gondola in Lutsen last year.', path: 'images/gondola.jpg', likes: 0 },
+    { id: 4, title: 'Loggers', description: 'About to tee off at a course I frequent, Loggers Trail.', path: 'images/loggers.jpg', likes: 0 },
+    { id: 5, title: 'Northern', description: 'Frances enjoying a treat in front of Northern Waters in Duluth.', path: 'images/northern.jpg', likes: 0 },
+    { id: 6, title: 'Tree', description: 'The family with our freshly harvested tree.', path: 'images/tree.jpg', likes: 0 }
 ];
 
 module.exports = galleryItems;
